dcc3b614bc22b1b3b3a954dd5901e4a2cb5a0926
wley3337 <wley3337@gmail.com>
2020-01-12 16:44:00 -0500

    added Worn model

---
+++ models/worn.py
@@ -0,0 +1,12 @@
+from app import db
+from sqlalchemy import ForeignKey
+
+
+class Worn(db.Model):
+    __tablename__ = 'worn'
+    id = db.Column(db.Integer, primary_key=True)
+    outfit_id = db.Column(db.Integer, ForeignKey('outfits.id'))
+    date_worn = db.Column(db.DateTime())
+    created_at = db.Column(db.DateTime(), server_default=db.func.now())
+    updated_at = db.Column(
+        db.DateTime(), server_default=db.func.now(), server_onupdate=db.func.now())
